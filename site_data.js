541d99178292de1b5edc711af8b31825af3b6299
dreamsboolean <91847161+dreamsboolean@users.noreply.github.com>
2021-10-03 17:11:47 -0700

    Update site_data.js

--- site_data.js
+++ site_data.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
     $(".business_name").attr("href","https://www.google.com/");
     $(".business_phone").text("123-123-1234");
     $(".business_phone").attr("href","tel:11231231234");
-    $(".business_phone").text("1123 SE Springfield Rd. Portland, Oregon 97233");
-    $(".business_phone").attr("href","https://www.google.com/maps/search/123+SE+Springfield+Rd.+Portland,+Oregon+97233/@45.5156195,-122.5387312,17z/data=!3m1!4b1");
-    $(".business_phone").text("apples4sale@example.com");
-    $(".business_phone").attr("href","mailto:apples4sale@example.com");
+    $(".business_address").text("1123 SE Springfield Rd. Portland, Oregon 97233");
+    $(".business_address").attr("href","https://www.google.com/maps/search/123+SE+Springfield+Rd.+Portland,+Oregon+97233/@45.5156195,-122.5387312,17z/data=!3m1!4b1");
+    $(".business_email").text("apples4sale@example.com");
+    $(".business_email").attr("href","mailto:apples4sale@example.com");
 });
 
 
