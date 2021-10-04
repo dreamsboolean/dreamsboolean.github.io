730bb7862a28f8989430dcf95bdddd9dec405486
dreamsboolean <91847161+dreamsboolean@users.noreply.github.com>
2021-10-03 17:30:49 -0700

    Update site_data.js

--- site_data.js
+++ site_data.js
@@ -1,12 +1,28 @@
 $(document).ready(function(){
+    // business name
     $(".business_name").text("John's Apple Farm");
     $(".business_name").attr("href","https://www.google.com/");
+    // business phone
     $(".business_phone").text("123-123-1234");
     $(".business_phone").attr("href","tel:11231231234");
+    //business address
     $(".business_address").text("1123 SE Springfield Rd. Portland, Oregon 97233");
     $(".business_address").attr("href","https://www.google.com/maps/search/123+SE+Springfield+Rd.+Portland,+Oregon+97233/@45.5156195,-122.5387312,17z/data=!3m1!4b1");
+    // business email
     $(".business_email").text("apples4sale@example.com");
     $(".business_email").attr("href","mailto:apples4sale@example.com");
+    // business facebook
+    $(".business_facebook").text("facebook place holder");
+    $(".business_facebook").attr("href","https://www.google.com");
+    // business instagram
+    $(".business_instagram").text("instagram place holder");
+    $(".business_instagram").attr("href","https://www.google.com");
+    // business twitter
+    $(".business_twitter").text("twitter place holder");
+    $(".business_twitter").attr("href","https://www.google.com");
+    // business youtube
+    $(".business_youtube").text("youtube place holder");
+    $(".business_youtube").attr("href","https://www.google.com");
 });
 
 
