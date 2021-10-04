f301d59e5e7e10a7b4ab89e0b809e8ec3952b1c1
dreamsboolean <91847161+dreamsboolean@users.noreply.github.com>
2021-10-03 17:11:06 -0700

    Update site_data.js

--- site_data.js
+++ site_data.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
     $(".business_name").text("John's Apple Farm");
     $(".business_name").attr("href","https://www.google.com/");
-    $(".business_phone").text("John's Apple Farm");
-    $(".business_phone").attr("href","https://www.google.com/");
- });
+    $(".business_phone").text("123-123-1234");
+    $(".business_phone").attr("href","tel:11231231234");
+    $(".business_phone").text("1123 SE Springfield Rd. Portland, Oregon 97233");
+    $(".business_phone").attr("href","https://www.google.com/maps/search/123+SE+Springfield+Rd.+Portland,+Oregon+97233/@45.5156195,-122.5387312,17z/data=!3m1!4b1");
+    $(".business_phone").text("apples4sale@example.com");
+    $(".business_phone").attr("href","mailto:apples4sale@example.com");
+});
+
+
+
+//<span class="business_facebook"></span>
+//<span class="business_instagram"></span>
+//<span class="business_twitter"></span>
+//<span class="business_youtube"></span>
