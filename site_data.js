93593effbf36f34bedb45be5d61670e6e8aa17f6
dreamsboolean <91847161+dreamsboolean@users.noreply.github.com>
2021-10-03 17:04:53 -0700

    Add files via upload

---
+++ site_data.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $(".business_name").text("John's Apple Farm");
+    $(".business_name").attr("href","https://www.google.com/");
+    $(".business_phone").text("John's Apple Farm");
+    $(".business_phone").attr("href","https://www.google.com/");
+ });
